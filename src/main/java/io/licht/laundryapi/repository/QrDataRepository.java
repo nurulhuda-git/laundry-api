41626d578293edf4fbc482856b3b161c961a4403
nurulhuda-git <artificialbaka@gmail.com>
2021-11-28 13:09:38 +0700

    create repository for qrData ORM

---
+++ src/main/java/io/licht/laundryapi/repository/QrDataRepository.java
@@ -0,0 +1,14 @@
+package io.licht.laundryapi.repository;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import io.licht.laundryapi.model.QrData;
+
+@Repository
+public interface QrDataRepository extends JpaRepository<QrData,UUID>
+{
+    
+}
